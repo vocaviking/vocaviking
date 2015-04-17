460dfec0c4ce4ee018a0403def1471cf71c7cee6
vocaviking <jan.sende@uni-jena.de>
2015-04-17 20:36:26 +0200

    Desktop App Preparation Part I
    Moved some stuff from the phone to the share folder, but still not done yet :(

---
+++ VocabularyTrainer/VocabularyTrainer.Shared/DataModel/vocabulary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internationalization
+{
+    class DictionaryClass
+    {
+        string Name;
+        List<LanguageClass> Languages;
+        List<WordClass> Translations;
+        //Language
+        //from
+        //to
+    }
+    class LanguageClass
+    {
+        string Name; //Internationalize?
+        AlphabetClass Alphabet;
+        GrammaticsClass Grammatics;
+    }
+    class AlphabetClass
+    {
+        string Name;
+        List<char> Characters;
+    }
+    class GrammaticsClass
+    {
+
+    }
+    class WordClass
+    {
+        int ID;
+        string Description;
+        string Etymology;
+        List<string> Groups;
+        //Wortart
+        //Wortstamm
+        //Sprache, von nach
+    }
+
+}
